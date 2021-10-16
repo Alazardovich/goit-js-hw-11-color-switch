71c580b2bd84bf0fe756833bde004fc488afb623
Artem <lazarazuy@gmail.com>
2021-10-16 20:31:31 +0200

    add repo

---
+++ index.js
@@ -0,0 +1,12 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+  const onStart = document.querySelector('button[data-action="start"]');
+  const onStop = document.querySelector('button[data-action="stop"]');
+  console.log(onStop);
