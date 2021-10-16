9d9ee82023a67e189d5f3d0bcaf50f142cac8cc1
Artem <lazarazuy@gmail.com>
2021-10-16 22:10:35 +0200

    add first

--- index.js
+++ index.js
@@ -9,4 +9,17 @@ const colors = [
 
   const onStart = document.querySelector('button[data-action="start"]');
   const onStop = document.querySelector('button[data-action="stop"]');
-  console.log(onStop);
+  const onBody = document.body;
+  console.log(onBody);
+  onStart.addEventListener('click', onRandomColor);
+
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  function onRandomColor() {
+    colors.forEach((color) => {
+      document.body.style.background = color[randomIntegerFromInterval]; 
+    })
+    
+  }
