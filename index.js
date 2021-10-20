150a76ed9934a7feda3ddb0c1473ad77c90a15e8
Artem <lazarazuy@gmail.com>
2021-10-20 22:17:11 +0200

    add color Switch

--- index.js
+++ index.js
@@ -7,19 +7,38 @@ const colors = [
     '#795548',
   ];
 
+
   const onStart = document.querySelector('button[data-action="start"]');
   const onStop = document.querySelector('button[data-action="stop"]');
   const onBody = document.body;
-  console.log(onBody);
-  onStart.addEventListener('click', onRandomColor);
 
+  const DEPLOY_TIME = 1000;
+  /*переменная для изменения Буля, чтобы остановить или true-запустить button*/
+  let hasAction = false;  
+  
+  onStart.addEventListener('click', onRandomColor);
+  onStop.addEventListener('click', offRandomColor);
+
+  let intervalId = null;
   const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+  
 
   function onRandomColor() {
-    colors.forEach((color) => {
-      document.body.style.background = color[randomIntegerFromInterval]; 
-    })
+    if (hasAction){
+      return;
+    }
+    intervalId = setInterval(()=>{
+      hasAction = true;
+      const randomIdx = randomIntegerFromInterval(0,colors.length -1)
+      onBody.style.backgroundColor = colors[randomIdx]
+
+    },1000)
     
-  }
+  };
+    
+  function offRandomColor() {
+    hasAction = false;
+   clearInterval(intervalId); 
+  };
