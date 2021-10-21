1a4d00f244521e9048d17836923f69c620c7fe71
Artem <lazarazuy@gmail.com>
2021-10-21 16:33:24 +0200

    change atribute btn

--- index.js
+++ index.js
@@ -14,31 +14,38 @@ const colors = [
 
   const DEPLOY_TIME = 1000;
   /*переменная для изменения Буля, чтобы остановить или true-запустить button*/
-  let hasAction = false;  
+  // let hasAction = false;  
   
   onStart.addEventListener('click', onRandomColor);
   onStop.addEventListener('click', offRandomColor);
+
 
   let intervalId = null;
   const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
-  
+  // onStart.desabled = true;
 
   function onRandomColor() {
-    if (hasAction){
+    if (onStart.desabled === true){
       return;
     }
     intervalId = setInterval(()=>{
-      hasAction = true;
+      // hasAction = true;
+      onStart.desabled = true;
+      onStop.desabled = false;
       const randomIdx = randomIntegerFromInterval(0,colors.length -1)
       onBody.style.backgroundColor = colors[randomIdx]
 
     },1000)
-    
+
   };
-    
+ 
   function offRandomColor() {
-    hasAction = false;
+    // hasAction = false;
+    onStart.desabled = false;
+    onStop.desabled = true;
    clearInterval(intervalId); 
   };
+// console.log(onStart);
+// console.log(onStop,offRandomColor());
